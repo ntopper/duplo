1ec063b002ba6aa0d8214522f9e1e81fbcfd7830
ntopper <nicktopper@gmail.com>
2015-12-18 13:06:32 -0500

    modified experement to fit the methods from the paper

--- legovision.py
+++ legovision.py
@@ -1,8 +1,23 @@
-#bin/bash python
+#/bin/bash python
+import argparse
 import numpy
 import cv2
 import serial
 import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-ll', dest="ll_side", default="left")
+parser.add_argument('-delay', dest="base_delay", default=1)
+parser.add_argument('-reward', dest="base_reward", default=2)
+
+args = parser.parse_args()
+
+ll_side = args.ll_side
+if ll_side == "right": ss_side = "left"
+else: ss_side = "right"
+
+base_delay = args.base_delay
+base_reward = args.base_reward
 
 cv2.namedWindow('', cv2.WINDOW_NORMAL)
 
@@ -29,119 +44,118 @@ class Region:
     def slice(self, frame):
         return frame[self.y[0]:self.y[1],self.x[0]:self.x[1]]
 
+
 def draw(frame, mask):
     for r in regions:
-        frame[r.y[0]:r.y[1],r.x[0]:r.x[1]] = r.slice(mask)
-        cv2.putText(frame, r.name, (r.x[0], r.y[1] - 5),cv2.FONT_HERSHEY_SIMPLEX, .4, 255)
+        cv2.rectangle(frame,
+                (r.x[0],r.y[0]),
+                (r.x[1],r.y[1]),
+                (0, 255 * r.slice(mask).any(),0),
+                3
+        )
+
+        cv2.putText(frame,
+                r.name,
+                (r.x[0] + 10, r.y[1] - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                .4,
+                (0,255,0)
+        )
+
     return frame
 
-ser = serial.Serial('/dev/cu.usbmodem1411', 9600, timeout=0)
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=0)
 
 REWARD_READY = False
+ll_adder = 0
 
-left_combo = 0
-right_combo = 0
-left_last_detected = 0
-right_last_detected = 0
+countdown_end = 0
+countdown_pending = False
+def ll():
+        global REWARD_READY
+        global countdown_pending
+        global ll_adder
+        global countdown_end
 
-def l():
-   global REWARD_READY
-   if not REWARD_READY:
-       return
+        if not REWARD_READY:
+                return
+        REWARD_READY = False
+        countdown_pending = True
+        print "LL triggered"
+        delay = base_delay + ll_adder
+        ser.write("feed_%s "%(ll) + str(delay))
 
-   global left_last_detected
-   global left_combo
-   global right_combo
+        countdown_end = time.time() + delay
 
-   left_last_detected = time.time()
-   
-   if (right_combo > 0):
-      right_combo -= 1
-   else:
-      right_combo =0
+def ss():
+        global REWARD_READY
+        global countdown_pending
+        global ll_adder
+        global countdown_end
 
-   if(left_combo > 0):
-      ser.write("feed_left %s\n"%str(left_combo + 1))
-      
-   else:
-      ser.write("feed_left 2\n")
+        if not REWARD_READY:
+                return
 
-   REWARD_READY = False
+        REWARD_READY = False
+        countdown_pending = True
 
-def r():
-   global REWARD_READY
-   if not REWARD_READY:
-       return
-
-   global left_combo
-   global right_combo
-   global right_last_detected
-
-   right_last_detected = time.time()
-
-   if (left_combo > 0):
-      left_combo -= 1
-   else:
-      left_combo = 0
-
-   right_combo += 1
-
-   if(right_combo > 0):
-      ser.write("feed_right %s\n"%str(right_combo))
-      
-   else:
-      ser.write("feed_right 1\n")
-
-   REWARD_READY = False
-
+        print "SS triggered"
+        ll_adder = max(0, ll_adder - 1)
+        ser.write("feed_%s "%(ss) + str(base_delay))
+        countdown_end = time.time() + base_delay
 
 def c():
-    
-    global REWARD_READY
-    REWARD_READY = True
+        global REWARD_READY
 
+        if not REWARD_READY:
+                print "new run"
+                print "next ll delay: %s seconds"%(str(base_delay + ll_adder))
 
+        REWARD_READY = True
 
-def tl():
+def ll_end():
+        global ll_adder
+        global countdown_pending
 
-    global left_combo
-    global left_last_detected
- 
-    ser.write('\n')
+        if not countdown_pending:
+            return
+        countdown_pending = False
 
-    if left_last_detected == 0:
-        return
+        ser.write('\n')
+        if time.time() > countdown_end:
+                ll_adder += 1
+                print "ll reward successfull"
+        else:
+            print "ll reward interupted"
+        print "next ll delay: %s seconds"%(str(base_delay + ll_adder))
 
-    if(time.time() - left_last_detected > left_combo):
-       left_combo += 1
-       left_last_detected = 0
+def ss_end():
 
-    print left_combo
+        global ll_adder
+        global countdown_pending
 
-def tr():
+        if not countdown_pending:
+                return
+        countdown_pending = False
 
-    global right_combo
-    global right_last_detected
- 
-    ser.write('\n')
-
-    if right_last_detected == 0:
-        return
-
-    if(time.time() - right_last_detected > right_combo):
-       right_combo += 1
-       right_last_detected = 0
-
-    print right_combo
+        ser.write('\n')
+        if time.time() > countdown_end:
+                ll_adder = max(0, ll_adder - 1)
+                print "ss reward successfull"
+        else:
+            print "ss reward interupted"
+        print "next ll delay: %s seconds"%(str(base_delay + ll_adder))
 
 #callback functions
-callback = {"left": l, "right": r,"center": c,"topleft": tl, "topright": tr}
+callback = {ll_side: ll,
+        ss_side: ss,
+        "center": c,
+        "top"+ll_side: ll_end,
+        "top"+ss_side: ss_end
+        }
 
 #list of regions
 regions = []
-
-#serial port
-#ser = serial.Serial('/dev/tty.usbserial', 9600)
 
 if __name__ == "__main__":
 
@@ -150,47 +164,39 @@ if __name__ == "__main__":
         #make list of Region objects from config file
         regions = [Region(l) for l in f.readlines()]
 
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     fgbg = cv2.BackgroundSubtractorMOG()
 
     #warm up the camera
     for i in xrange(60):
         ret, frame = cap.read()
 
-
     while(True):
-        
-	try:
-		# Capture frame-by-frame
-		ret, frame = cap.read()
 
-		#greyscale version of image
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # Capture frame-by-frame
+            ret, frame = cap.read()
 
-		#bg subtract from blured greyscale
-		fgmask = fgbg.apply(cv2.blur(gray, (20,20)))
+            #greyscale version of image
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-		#check each region for movement
-		for r in regions:
-		    if r.detect(fgmask):
+            #bg subtract from blured greyscale
+            fgmask = fgbg.apply(cv2.blur(gray, (20,20)))
 
-			#execute callback function
-			callback[r.name]()
+            #check each region for movement
+            for r in regions:
+                    if r.detect(fgmask):
 
-			#notify serial port
-			#ser.write(r.name)
-			#long poll, I.E. wait for response
-			#ser.readline()
+                            #execute callback function
+                            callback[r.name]()
 
-		#draw overlays
-		draw(gray, fgmask)
+            #draw overlays
+            draw(frame, fgmask)
 
-		# Display the resulting frame
-		cv2.imshow('', gray)
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-		    break
-        except:
-            pass
+            # Display the resulting frame
+            cv2.imshow('', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
     # When everything done, release the capture
     cap.release()
     cv2.destroyAllWindows()
